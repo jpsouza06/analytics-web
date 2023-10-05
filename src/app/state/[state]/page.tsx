12a596ca6dcf69beacec98864b0911c0d98cae21
Joao Paulo <joao.souza@atsinformatica.com.br>
2023-10-05 17:23:34 -0300

    fix bug in deploy

--- src/app/state/[state]/page.tsx
+++ src/app/state/[state]/page.tsx
@@ -1,5 +1,6 @@
-import { POST } from '@/app/api/route'
 import Grid from '@/components/grid'
+import { ISystemStartedResponse } from '@/interface/system-started'
+import dayjs from 'dayjs'
 
 import React from 'react'
 
@@ -15,4 +16,44 @@ export default async function State({params}: {params : {state: string}}) {
 			</div>		
 		</>
 	)
+}
+
+async function POST(state: string, page: number) {
+	const response = 
+		await fetch(`${process.env.API_BASE_URL}/system-started/query/${page}`, 
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(
+					{ 
+						estado: state,
+						dataInicio: '01-01-2000',
+						// orderBy: {
+						// 	createdAt: 'desc'
+						// }
+					}),
+				next: { revalidate: 60 },
+			})
+
+	if (response.status !== 200) {
+		const data = {
+			systemStarted: []
+		}
+
+		return {
+			data
+		}
+	}
+
+	const data: ISystemStartedResponse = await response.json()
+	
+	data.systemStarted.forEach(item => 
+		item.createdAt = dayjs(item.createdAt).format('DD/MM/YYYY')	
+	)
+
+	return {
+		data
+	}
 }
