c2b4103a7bc3641ed727de515a002f32291397f7
João Paulo <joaopaulosc06@gmail.com>
2023-10-28 11:17:36 -0300

    add next enviroment variables

---
+++ src/env.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+	PORT: z.coerce.number().default(3000),
+	API_BASE_URL: z.string().url(),
+	NEXT_PUBLIC_SITE_NAME: z.string()
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if(!parsedEnv.success) {
+	console.error(
+		'Invalid environment variables', 
+		parsedEnv.error.flatten().fieldErrors
+	)
+
+	throw new Error('Invalid environment variables')
+}
+
+export const env = parsedEnv.data
